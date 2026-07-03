4c92a800b40ea301db98d4ea8c4a59bae9e1087d
senthilkumar112 <sattur112@gmail.com>
2026-07-03 14:44:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('Portfolio loaded');
+// Dark Mode
+
+const mode = document.getElementById("mode");
+
+mode.onclick = function(){
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+mode.innerHTML="☀️";
+
+}else{
+
+mode.innerHTML="🌙";
+
+}
+
+};
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
+
+// Fade-in Animation
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity=1;
+
+entry.target.style.transform="translateY(0px)";
+
+}
+
+});
+
+});
+
+document.querySelectorAll("section").forEach(sec=>{
+
+sec.style.opacity=0;
+
+sec.style.transform="translateY(40px)";
+
+sec.style.transition="all 0.8s ease";
+
+observer.observe(sec);
+
+});
